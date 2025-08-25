bcc2b1f3fba8b596572655fc66b926c77ba34aa5
Valtren Corp <falahwajarullah@gmail.com>
2025-08-25 08:13:49 +0700

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1>Valtren Corp HQ</h1>
+      <p>Build udah ijo, saatnya gas deploy 🚀</p>
+    </main>
+  );
+}
