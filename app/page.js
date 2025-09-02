823530c167d43e74bd389a555a5cddfcc8bb4448
Valtren Corp <falahwajarullah@gmail.com>
2025-09-02 21:54:26 +0700

    Delete app/page.js

--- app/page.js
+++
@@ -1,7 +0,0 @@
-// TEMP HOMEPAGE MINIMAL (tanpa import/link/map)
-export const dynamic = 'force-static';
-export const revalidate = 3600;
-
-export default function Page() {
-  return <main style={{ padding: 24 }}>OK</main>;
-}
